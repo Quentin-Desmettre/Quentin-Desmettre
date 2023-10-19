86b8cf502ee6fc182e0649a41c8b6ae9779402b5
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2023-10-19 11:20:50 +0200

    ci: Push application to VPS instead of github pages

---
+++ redirect_http/redirect.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(301,{Location: `https://${req.headers.host}${req.url}`});
+  res.end();
+});
+
+server.listen(80);
+console.log(`http2https ==> 80:443`);
