764e9a678214b080f96efaf2a14c0c6d1ea96618
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2024-03-12 01:13:16 +0100

    refacto: Changed from javascript to typescript template

--- redirect_http/redirect.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(301,{Location: `https://${req.headers.host}${req.url}`});
-  res.end();
-});
-
-server.listen(80);
-console.log(`http2https ==> 80:443`);
